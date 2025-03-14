6c9236ea10925d0dc21a86a6964c32edb6b83f32
choesuhyeon276 <choesuhyeon276@gmail.com>
2025-03-15 03:41:11 +0900

    index.js 추가

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>장비 예약 시스템</h1>
+        <p>예약하려면 <a href="/reserve">/reserve 페이지</a>로 이동하세요!</p>
+      </div>
+    );
+  }
+  
