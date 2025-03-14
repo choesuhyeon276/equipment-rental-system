9fb9132cd85384bf19906eb4981e3629b9ca2e15
choesuhyeon276 <choesuhyeon276@gmail.com>
2025-03-15 03:54:01 +0900

    홈페이지 추가

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
     return (
       <div>
         <h1>장비 예약 시스템</h1>
-        <p>예약하려면 <a href="/reserve">/reserve 페이지</a>로 이동하세요!</p>
+        <p>예약하려면 <a href="/reserve">여기 클릭</a>하세요!</p>
       </div>
     );
   }
